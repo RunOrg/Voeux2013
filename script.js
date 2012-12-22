9b10e0d24e16c5c1b688336468974496cb1d896e
Victor Nicollet <victor@nicollet.net>
2012-12-22 12:13:42 +0100

    Timing, section 2 aspect

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ $(function(){
 	var last = 0;
 	setTimeout(show('s1'),last += 0);
 	setTimeout(hide('s1'),last += 1800 + 3000);
-	setTimeout(show('s2'),last += 1800);
+	setTimeout(show('s2'),last += 2160);
 });
